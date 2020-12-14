21414c79c9bf7f7e15310adf7b51bf3c01b73565
Cowleyfitcode <65091695+Mcowley1@users.noreply.github.com>
2020-12-13 23:43:39 -0500

    uploaded readme

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# friendsThoughts
+# friends-Thoughts API
+## Summary
+ For this project, I built an API on the backend for a social network web application. This project allows users and friends to react to each others thoughts and create a list of friends.
+
+## Languages and npm packages
+> 1. JavaScript
+> 2. MongoDB
+> 3. Mongoose
+> 4. node
+> 5. express
+
+## Installation
+> 1. Clone repo
+> 2. command line run npm install
+> 3. Type npm start to run server
+> 4. server is connected to localhost:3001, run routes in insomnia core for testing
+
+## Screenshot 
+![insomnia-core](screenshot.png)
+
+## Walk through
---
+++ models/User.js
@@ -0,0 +1,57 @@
+const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+// User Schema
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: 'You need to provide your username.',
+            trim: true
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: 'You need to provide your valid email address.',
+            validate: {
+                validator(validEmail) {
+                  return /^([a-zA-Z0-9_\.-]+)@([\da-z\.-]+)\.([a-z]{2,6})(\.[a-z]{2,6})?$/.test(
+                    validEmail
+                  );
+                },
+                message: "Please enter your valid email address",
+              },
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }    
+);
+
+// Virtual friend count
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+})
+
+
+const User = model('User', UserSchema);
+
+// Export 
+module.exports = User;
